33aab913325aa28d7a51160486338578fc8de6cf
João Pinto [1201765] <1201765@isep.ipp.pt>
2021-05-03 11:04:38 +0100

    added changes in conform with https://bitbucket.org/atb/tut-basic-gradle

---
+++ Ca3/part_2/tut-basic-gradle/src/main/java/com/greglturnquist/payroll/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.greglturnquist.payroll;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ReactAndSpringDataRestApplication.class);
+    }
+
+}
